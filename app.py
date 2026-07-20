c14c494725005cc1ded3bde8f0c718a5a19f3315
rohith vuppula <rohithvuppula@gmail.com>
2026-07-20 23:46:45 +0530

    refactor: keep app.py clean WSGI entry point for Gunicorn

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 import sys
 import os
 
-# Enable gevent & psycogreen non-blocking async I/O
-try:
-    import gevent.monkey
-    gevent.monkey.patch_all(thread=False)
-    import psycogreen.gevent
-    psycogreen.gevent.patch_psycopg()
-except Exception:
-    pass
-
 # Add backend folder to python search path
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backend'))
 
