6bd398b80cc9d24a4bf3fc80dabc976ef1edcb96
rohith vuppula <rohithvuppula@gmail.com>
2026-07-22 14:29:45 +0530

    fix: copy inner assessment files directly to /var/www/assessments root to fix 502 Bad Gateway

--- terraform_hostinger/main.tf
+++ terraform_hostinger/main.tf
@@ -41,13 +41,12 @@ resource "null_resource" "hostinger_vps_deploy" {
 
       "echo '=== [3/8] Fetching Latest ElevateIQ & Assessments Repositories ==='",
       "if [ ! -d '/var/www/elevateiq/.git' ]; then GIT_TERMINAL_PROMPT=0 git clone https://github.com/rohith1246/ELEVATEIQFINAL.git /var/www/elevateiq; else cd /var/www/elevateiq && git pull origin main; fi",
-      "if [ ! -d '/var/www/assessments/.git' ]; then GIT_TERMINAL_PROMPT=0 git clone https://github.com/shivapendala/assessments.git /var/www/assessments; else cd /var/www/assessments && git pull origin main; fi",
+      "rm -rf /tmp/assessments_tmp && GIT_TERMINAL_PROMPT=0 git clone https://github.com/shivapendala/assessments.git /tmp/assessments_tmp && cp -rf /tmp/assessments_tmp/assessments/* /var/www/assessments/ && rm -rf /tmp/assessments_tmp",
 
       "echo '=== [4/8] Building Python Virtual Environments & Seeding DB ==='",
       "cd /var/www/elevateiq && python3 -m venv venv && /var/www/elevateiq/venv/bin/pip install --upgrade pip && /var/www/elevateiq/venv/bin/pip install -r requirements.txt",
-      "sudo mkdir -p /var/www/assessments/instance",
-      "sudo chown -R $USER:$USER /var/www/assessments",
-      "cd /var/www/assessments && python3 -m venv venv && /var/www/assessments/venv/bin/pip install --upgrade pip gunicorn gevent greenlet && /var/www/assessments/venv/bin/pip install -r requirements.txt || true",
+      "sudo mkdir -p /var/www/assessments/instance && sudo chown -R $USER:$USER /var/www/assessments",
+      "cd /var/www/assessments && python3 -m venv venv && /var/www/assessments/venv/bin/pip install --upgrade pip gunicorn gevent greenlet && /var/www/assessments/venv/bin/pip install -r requirements.txt",
       "sudo cp /var/www/elevateiq/.env /var/www/assessments/.env || true",
       "cd /var/www/assessments && FLASK_APP=\"app:create_app()\" /var/www/assessments/venv/bin/flask init-db || true",
       "cd /var/www/assessments && /var/www/assessments/venv/bin/python seed_jd_assessment.py || true",
