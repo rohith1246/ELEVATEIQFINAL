52ef9298c55388c191aeb5d1a804015de5c977e4
rohith vuppula <rohithvuppula@gmail.com>
2026-07-22 14:34:31 +0530

    fix: add subfolder cp step to ensure assessment files are present at /var/www/assessments/

--- terraform_hostinger/main.tf
+++ terraform_hostinger/main.tf
@@ -42,9 +42,11 @@ resource "null_resource" "hostinger_vps_deploy" {
       "echo '=== [3/8] Fetching Latest ElevateIQ & Assessments Repositories ==='",
       "if [ ! -d '/var/www/elevateiq/.git' ]; then GIT_TERMINAL_PROMPT=0 git clone https://github.com/rohith1246/ELEVATEIQFINAL.git /var/www/elevateiq; else cd /var/www/elevateiq && git pull origin main; fi",
       "if [ ! -d '/var/www/assessments/.git' ]; then GIT_TERMINAL_PROMPT=0 git clone https://github.com/shivapendala/assessments.git /var/www/assessments; else cd /var/www/assessments && git pull origin main; fi",
+      "if [ -d '/var/www/assessments/assessments' ]; then cp -rf /var/www/assessments/assessments/* /var/www/assessments/; fi",
 
       "echo '=== [4/8] Building Python Virtual Environments & Seeding DB ==='",
       "cd /var/www/elevateiq && python3 -m venv venv && /var/www/elevateiq/venv/bin/pip install --upgrade pip && /var/www/elevateiq/venv/bin/pip install -r requirements.txt",
+      "sudo mkdir -p /var/www/assessments/instance && sudo chown -R $USER:$USER /var/www/assessments",
       "cd /var/www/assessments && python3 -m venv venv && /var/www/assessments/venv/bin/pip install --upgrade pip gunicorn gevent greenlet && /var/www/assessments/venv/bin/pip install -r requirements.txt",
       "sudo cp /var/www/elevateiq/.env /var/www/assessments/.env || true",
       "cd /var/www/assessments && FLASK_APP=\"app:create_app()\" /var/www/assessments/venv/bin/flask init-db || true",
