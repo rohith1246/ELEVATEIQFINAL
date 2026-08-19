8e77f60ad0c83e13d8f67baaf5ae99074b1be264
rohith vuppula <rohithvuppula@gmail.com>
2026-08-19 20:46:54 +0530

    Update project files and repository configuration

--- terraform_hostinger/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "hostinger_vps_ip" {
-  description = "Target Hostinger VPS Public Address"
-  value       = var.hostinger_ip
-}
-
-output "deployment_status" {
-  description = "Deployment Completion Message"
-  value       = "ElevateIQ is running live on https://${var.domain_name}"
-}
